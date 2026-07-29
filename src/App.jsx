d75d9da28a72200440891515bec4d6baff4e61df
HerbertNtim <herbertntim15@gmail.com>
2026-07-29 14:21:19 +0000

    added components and constants

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { gsap } from "gsap";
     
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
   return (
-    <div className="text-3xl text-rose-400">App</div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
