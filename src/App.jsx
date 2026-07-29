8ee0bf193b5c6dc40b4a33ccfbb07942f442d933
HerbertNtim <herbertntim15@gmail.com>
2026-07-29 13:35:57 +0000

    setup initiail app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
 const App = () => {
   return (
     <div>App</div>
