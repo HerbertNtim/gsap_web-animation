ecd503f195903da5967f0c6956e1022e7d7bbeb9
HerbertNtim <herbertntim15@gmail.com>
2026-07-31 05:22:39 +0000

    added list to sections

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,7 +1,58 @@
+import { cocktailLists } from "../constants";
+
 const Cocktails = () => {
   return (
-    <div>Cocktails</div>
-  )
-}
+    <section id="cocktails" className="noisy">
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left leaf"
+        id="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right leaf"
+        id="c-right-leaf"
+      />
 
-export default Cocktails
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails:</h2>
+
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="md:me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Most loved cocktails:</h2>
+
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cocktails;
