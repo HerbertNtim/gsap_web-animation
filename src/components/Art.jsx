0c0372dd8715de2e31830488cb7737c05efbb4cc
HerbertNtim <herbertntim15@gmail.com>
2026-08-01 16:26:12 +0000

    set up art section

---
+++ src/components/Art.jsx
@@ -0,0 +1,12 @@
+const Art = () => {
+  return (
+    <section id="art">
+      <div className='container mx-auto h-full pt-20'>
+        <h2>The Art</h2>
+        
+      </div>
+    </section>
+  )
+}
+
+export default Art
