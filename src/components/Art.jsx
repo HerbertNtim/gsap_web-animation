4501fa79aae403db2b20ba937941b984f6eedb84
HerbertNtim <herbertntim15@gmail.com>
2026-08-01 17:01:07 +0000

    implemented the art section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,6 +1,31 @@
+import { useMediaQuery } from "react-responsive";
 import { featureLists, goodLists } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Art = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const start = isMobile ? "top 20%" : "top top";
+
+    const maskTimeline = gsap.timeline({
+      ScrollTrigger: {
+        trigger: "#art",
+        start,
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    maskTimeline.to(".will-fade", {
+      opacity: 0,
+      stagger: 0.2,
+      ease: "power1.inOut",
+    }).to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '400%', duration: 1, ease: 'power1.inOut'}).to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut'})
+  });
+
   return (
     <section id="art">
       <div className="container mx-auto h-full pt-20">
